fdea83008377c0b3830a142e71c136c125d6ca38
Doyeon Kim <60147434+doyeon111@users.noreply.github.com>
2022-05-30 20:33:42 +0900

    0530

---
+++ day0530/book/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.dao.BookDAO;
+
+
+@Controller
+public class BookController {
+	
+	@Autowired
+	private BookDAO dao;
+
+	public void setDao(BookDAO dao) {
+		this.dao = dao;
+	}
+	
+	@RequestMapping("/list.do")
+	public void list(Model model) {
+		model.addAttribute("list", dao.listBook());
+	}
+}
